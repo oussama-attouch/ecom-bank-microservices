8f6ad9475d1ae23f286c883ba8be4b00de9ff576
OUSSAMA ATTOUCH <73898127+Oussama-Att@users.noreply.github.com>
2026-09-18 18:18:48 +0100

    feat: set up discovery and config services

---
+++ config-service/src/test/java/att/ossama/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package att.ossama.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ discovery-service/src/test/java/att/ossama/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package att.ossama.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
